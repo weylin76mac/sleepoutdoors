decc1180d97d2d1d35c68df1afe7ee8980e2fb7d
schoolmosiahvb <168146560+schoolmosiahvb@users.noreply.github.com>
2024-05-12 12:18:05 -0600

    add

--- src/js/product.js
+++ src/js/product.js
@@ -2,12 +2,10 @@ import { setLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
 function addProductToCart(product) {
-  let items = getLocalStorage("so-cart");
-  if (!Array.isArray(items)) {
-    items = [];
-  }
-  items.push(product);
-  setLocalStorage("so-cart", items);
+  //Because localStorage only deals with strings, and we have an object, we set getLocalStorage to default to an array, then push the product to the array. Same process repeated in cart.js
+  const currCart = getLocalStorage("so-cart") || [];
+  currCart.push(product);
+  setLocalStorage("so-cart", currCart);
 }
 
 // add to cart button event handler
