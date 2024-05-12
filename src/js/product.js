5fcd0899b1a9a312abf7fea6ba9678271407ba9f
Weylin76 <Weylin76@msn.com>
2024-05-12 19:32:16 -0400

    Weylin delete btn branch chnages

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,11 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  //Because localStorage only deals with strings, and we have an object, we set getLocalStorage to default to an array, then push the product to the array. Same process repeated in cart.js
+  const currCart = getLocalStorage("so-cart") || [];
+  currCart.push(product);
+  setLocalStorage("so-cart", currCart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
