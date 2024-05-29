40d1cb880799fc5b3aefd5d94e3b05913e54d589
schoolmosiahvb <168146560+schoolmosiahvb@users.noreply.github.com>
2024-05-28 22:54:31 -0600

    finishing up

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,13 @@ import { loadHeaderFooter } from "./utils.mjs";
 loadHeaderFooter();
 
 productList(".product-list", "tents");
+
+// Mosiah added for the Newsletter Homepage button 
+document.addEventListener("DOMContentLoaded", () => {
+    const signupButton = document.getElementById("signup-button");
+    if (signupButton) {
+      signupButton.addEventListener("click", () => {
+        location.href = "other_pages/newsletter.html";
+      });
+    }
+  });
