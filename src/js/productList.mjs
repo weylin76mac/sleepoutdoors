e60f25fcde44956c2cdc600a1d6309675ca89bb7
milamelo4 <mel19009@byui.edu>
2024-05-30 17:02:44 -0600

    Finished stretch goal 2

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -11,17 +11,19 @@ export function discount(SuggestedRetailPrice, FinalPrice) {
 function productCardTemplate(product) {
   const disc = discount(product.SuggestedRetailPrice,product.FinalPrice)
  
-    return `<li class="product-card">
+    return `
+    <li class="product-card">
       <a href="product_pages/index.html?product=${product.Id}">
       <img
-        src="${product.Images.PrimaryExtraLarge}"
+        src="${product.Images.PrimaryMedium}"
         alt="Image of ${product.Name}"
       />
       <h3 class="card__brand">${product.Brand.Name}</h3>
       <h2 class="card__name">${product.NameWithoutBrand}</h2>
       <p class="product-card__price">$${product.FinalPrice}</p>
       <p class= "cart-card__discount">${disc}% Off. Save Today!</p></a>
-    </li>`;
+    </li>
+    `;
   }
 
 export default async function productList(selector, category) {
@@ -33,7 +35,12 @@ export default async function productList(selector, category) {
     console.log(product);
     // render out the product list to the element
     renderListWithTemplate(productCardTemplate, element, product);
-    document.querySelector(".title").innerHTML = category;
+    let cap = capitalize(category)
+    document.querySelector(".title").innerHTML = cap;
+}
+
+function capitalize(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1)
 }
 
 
