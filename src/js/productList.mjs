2c6fe961472bc8e8fed71aca84cdfd30f38dbf76
milamelo4 <mel19009@byui.edu>
2024-06-03 16:33:41 -0600

    added 2 digit FinalPrice for productList

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -14,7 +14,7 @@ function productCardTemplate(product) {
       <h3 class="card__brand">${product.Brand.Name}</h3>
       <h2 class="card__name">${product.NameWithoutBrand}</h2>
       <p class="oldPrice">$${product.SuggestedRetailPrice.toFixed(2)}</p>
-      <p class="product-card__price">$${product.FinalPrice}</p>
+      <p class="product-card__price">$${product.FinalPrice.toFixed(2)}</p>
       
     </li>
     `;
